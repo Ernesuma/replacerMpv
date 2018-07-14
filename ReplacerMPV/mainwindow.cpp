d851647a6ab0e0d581759851a687b5781391cbf3
peter <peter@petermail.com>
2018-07-14 17:40:41 +0200

    changed connect calls from one line to two lines for better readability

--- ReplacerMPV/mainwindow.cpp
+++ ReplacerMPV/mainwindow.cpp
@@ -11,15 +11,23 @@ MainWindow::MainWindow(QWidget *parent) :
     createMenus();
 
     // route the needed gui widgets signals to the mainwindows signals
-    QObject::connect(ui->pushButton_replace, SIGNAL(clicked()), this, SIGNAL(pushBtnReplace_clicked()));
-    QObject::connect(ui->pushButton_c2c_plain, SIGNAL(clicked()), this, SIGNAL(pushBtnC2cPlain_clicked()));
-    QObject::connect(ui->pushButton_exportFinal, SIGNAL(clicked()), this, SIGNAL(pushBtnExportFinal_clicked()));
-    QObject::connect(ui->pushButton_c2c_final, SIGNAL(clicked()), this, SIGNAL(pushBtnC2cFinal_clicked()));
-    QObject::connect(ui->pushButton_add_2_tag_list, SIGNAL(clicked()), this, SIGNAL(pushBtnAddTag_clicked()));
-    QObject::connect(ui->pushButton_remove_selected_tags, SIGNAL(clicked()), this, SIGNAL(pushBtnRemoveSelectedTags_clicked()));
-    QObject::connect(ui->pushButton_remove_all_tags, SIGNAL(clicked()), this, SIGNAL(pushBtnRemoveAllTags_clicked()));
+    QObject::connect(ui->pushButton_replace, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnReplace_clicked()));
+    QObject::connect(ui->pushButton_c2c_plain, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnC2cPlain_clicked()));
+    QObject::connect(ui->pushButton_exportFinal, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnExportFinal_clicked()));
+    QObject::connect(ui->pushButton_c2c_final, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnC2cFinal_clicked()));
+    QObject::connect(ui->pushButton_add_2_tag_list, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnAddTag_clicked()));
+    QObject::connect(ui->pushButton_remove_selected_tags, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnRemoveSelectedTags_clicked()));
+    QObject::connect(ui->pushButton_remove_all_tags, SIGNAL(clicked()),
+                     this, SIGNAL(pushBtnRemoveAllTags_clicked()));
 
-    QObject::connect(ui->textEdit_plain, SIGNAL(textChanged()), this, SIGNAL(textEditPlain_textChanged()));
+    QObject::connect(ui->textEdit_plain, SIGNAL(textChanged()),
+                     this, SIGNAL(textEditPlain_textChanged()));
 }
 
 MainWindow::~MainWindow()
