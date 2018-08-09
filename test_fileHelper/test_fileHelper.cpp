9e09c95d8119b9691774e84419642bf68972a797
peter <peter@petermail.com>
2018-08-10 01:27:43 +0200

    added the readFile2TagMap unit test

--- test_fileHelper/test_fileHelper.cpp
+++ test_fileHelper/test_fileHelper.cpp
@@ -14,6 +14,7 @@ public:
 
 private Q_SLOTS:
     void test_getTagMapSeparator();
+    void test_readFile2TagMap();
 };
 
 Test_fileHelper::Test_fileHelper()
@@ -28,6 +29,32 @@ void Test_fileHelper::test_getTagMapSeparator()
 {
     QCOMPARE(FileHelper::getTagMapSeparator(),
              QChar(','));
+}
+
+/*
+ * test the read file 2 tag map method
+ */
+void Test_fileHelper::test_readFile2TagMap()
+{
+    // use file defined in the Resources
+    QDir filePath{QDir(":testdata/tagMap.testData")};
+
+    // call DUT to get the tag map
+    TagMapModel::tagMap tagMap;
+    FileHelper::ResultCode result = FileHelper::readFile2TagMap(filePath, tagMap);
+
+    // check the result code
+    // Todo: write tests to trigger the other result codes
+    QCOMPARE(result, FileHelper::ResultCode::OK);
+
+    // define the expected tag map data
+    // Todo: create more challanging test data
+    TagMapModel::tagMap tagMapExpected;
+    tagMapExpected.insert("Foo", "Bar");
+    tagMapExpected.insert("foo", "bar");
+
+    // compare read and expected tag map
+    QCOMPARE(tagMap, tagMapExpected);
 }
 
 QTEST_APPLESS_MAIN(Test_fileHelper)
